f54061aff4059d449bdba8326d6bb3c271b8f8f3
Indu Sree <Indu Sree@DESKTOP-UN8GH21>
2024-08-12 04:06:14 +0530

    Add project files.

---
+++ Constructors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Employee
+    {
+        public string Name;  
+
+        public Employee()
+        {
+            Name = "Indu Sree";  // set the value for Name
+        }
+
+        static void Main(string[] args)
+        {
+            Employee e = new Employee();  // Create an object of the Employee Class
+
+            Console.WriteLine(e.Name);  // Print the value of Name
+        }
+    }
+}
